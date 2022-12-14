fce2e266c60427b2266fe1f35aa8e2c2e0fb422f
Babi Mumba <babimumba243@gmail.com>
2022-12-15 00:51:54 +0200

    add enw file

--- app/src/main/java/com/BabiMumba/Esis_app/fragment/HomeFragment.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/fragment/HomeFragment.kt
@@ -1,19 +1,83 @@
 package com.BabiMumba.Esis_app.fragment
 
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import com.BabiMumba.Esis_app.R
+import com.BabiMumba.Esis_app.home.ActualiteActivity
+import com.BabiMumba.Esis_app.home.LectureActivity_Pdf
+import com.BabiMumba.Esis_app.home.ResultatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class HomeFragment : Fragment() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val viewF = inflater.inflate(R.layout.fragment_home, container, false)
+
+        viewF.findViewById<CardView>(R.id.syllabus_card).setOnClickListener {
+            startActivity(Intent(activity, Syllabus_FragmentActivity::class.java))
+        }
+        viewF.findViewById<CardView>(R.id.horaire_cat).setOnClickListener {
+            val actulaity_link = "https://www.esisalama.com/index.php?module=horaire"
+            val intent = Intent(activity, ActualiteActivity::class.java)
+            intent.putExtra("url_link",actulaity_link)
+            startActivity(intent)
+        }
+        viewF.findViewById<CardView>(R.id.esis_web).setOnClickListener {
+            val actulaity_link = "https://www.esisalama.com/index.php"
+            val intent = Intent(activity, ActualiteActivity::class.java)
+            intent.putExtra("url_link",actulaity_link)
+            startActivity(intent)
+        }
+        viewF.findViewById<CardView>(R.id.news_card).setOnClickListener {
+            val actulaity_link = "https://www.esisalama.com/index.php?module=actualite"
+            val intent = Intent(activity, ActualiteActivity::class.java)
+            intent.putExtra("url_link",actulaity_link)
+            startActivity(intent)
+        }
+        viewF.findViewById<CardView>(R.id.resultat_cat).setOnClickListener {
+            startActivity(Intent(activity, ResultatActivity::class.java))
+        }
+        viewF.findViewById<CardView>(R.id.lecture_cat).setOnClickListener {
+            startActivity(Intent(activity, LectureActivity_Pdf::class.java))
+        }
+        readname(viewF)
+        return viewF
+    }
+    fun readname(v:View){
+
+        val firebaseUser = firebaseAuth.currentUser
+        val mail = firebaseUser?.email.toString()
+        val db = FirebaseFirestore.getInstance()
+        val docRef = db.collection("Utilisateurs").document(mail)
+        docRef.get()
+            .addOnSuccessListener {
+                if (it!=null){
+                    val pren = it.data?.getValue("prenom").toString()
+                    v.findViewById<TextView>(R.id.prenom).text = pren
+
+                }else{
+                    Log.d(ContentValues.TAG,"no such document")
+                }
+            }
+
+            .addOnFailureListener {
+                Toast.makeText(requireActivity(), "${it}", Toast.LENGTH_SHORT).show()
+            }
+
     }
 }
---
+++ app/src/main/java/com/BabiMumba/Esis_app/home/ActualiteActivity.kt
@@ -0,0 +1,129 @@
+package com.BabiMumba.Esis_app.home
+
+import android.app.Dialog
+import android.app.DownloadManager
+import android.app.ProgressDialog
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Bitmap
+import android.net.ConnectivityManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.webkit.URLUtil
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import com.BabiMumba.Esis_app.R
+import kotlinx.android.synthetic.main.activity_actualite.*
+
+class ActualiteActivity : AppCompatActivity() {
+    lateinit var webView: WebView
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val action = intent?.action
+            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE == action) {
+                Toast.makeText(this@ActualiteActivity, "Telechargement terminer", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actualite)
+
+        if (isConnectedNetwork(this)){
+
+        }else{
+            web_actualit.visibility = View.GONE
+            showDialog()
+        }
+
+        webView = findViewById(R.id.web_eventmtn)
+
+        val lien = intent.getStringExtra("url_link").toString()
+        registerReceiver(broadcastReceiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+
+        val progressBar = ProgressDialog(this)
+        progressBar.setTitle("Patienter...")
+        progressBar.setMessage("chargement de la page")
+        progressBar.setCancelable(false)
+        progressBar.show()
+
+        web_eventmtn.settings.javaScriptEnabled = true
+
+        web_eventmtn!!.settings.builtInZoomControls = true
+        web_eventmtn.webViewClient = object : WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressBar.show()
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.dismiss()
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView, url: String?): Boolean {
+                view.loadUrl(url!!)
+                return true
+            }
+        }
+
+
+        web_eventmtn.loadUrl(lien)
+        web_eventmtn.setDownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
+            val request = DownloadManager.Request(
+                Uri.parse(url)
+            )
+            val filename = URLUtil.guessFileName(url, contentDisposition, mimetype);
+            request.allowScanningByMediaScanner()
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED) //Notify client once download is completed!
+            request.setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_DOWNLOADS,
+                filename
+            )
+            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+            Toast.makeText(
+                applicationContext,
+                "Telechargement de l'horaire",  //To notify the Client that the file is being downloaded
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+
+    }
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    fun isConnectedNetwork(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return cm.activeNetworkInfo != null && cm.activeNetworkInfo!!.isConnectedOrConnecting
+    }
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.non_internet)
+        dialog.setCancelable(false)
+        val lp = WindowManager.LayoutParams()
+        lp.copyFrom(dialog.window!!.attributes)
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT
+        dialog.findViewById<View>(R.id.quit).setOnClickListener { v: View? -> onBackPressed() }
+        // dialog.findViewById<View>(R.id.btn_feedback).setOnClickListener { v: View? -> startActivity(settg)}
+        dialog.show()
+        dialog.window!!.attributes = lp
+    }
+}
