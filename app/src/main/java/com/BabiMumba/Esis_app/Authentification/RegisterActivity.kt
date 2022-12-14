24102fd1bec2afb95c67f2fb3ad589e6f0ee20d9
Babi Mumba <babimumba243@gmail.com>
2022-12-14 22:06:25 +0200

    add enw fil

--- app/src/main/java/com/BabiMumba/Esis_app/Authentification/RegisterActivity.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/Authentification/RegisterActivity.kt
@@ -1,14 +1,20 @@
 package com.BabiMumba.Esis_app.Authentification
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Patterns
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.BabiMumba.Esis_app.R
 import com.BabiMumba.Esis_app.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.StorageReference
+
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -20,7 +26,45 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        clikbtn()
 
+    }
+    private fun clikbtn(){
+
+        binding.promotionText.setOnClickListener {
+            choixpromo()
+        }
+        binding.btnSignup.setOnClickListener {
+            if (isValidSignUpDetails()){
+                Toast.makeText(this, "tout est ok", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.layoutImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            startActivityForResult(
+                Intent.createChooser(
+                    intent,
+                    "Selectionner une image"
+                ), 101
+            )
+
+        }
+
+        binding.genre.setOnClickListener {
+            choigenre()
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val imageUri : Uri
+        if (requestCode == 101 && resultCode == RESULT_OK) {
+            imageUri = data?.data!!
+            filepath = data.data!!
+            binding.imageProfil.setImageURI(imageUri)
+            binding.txtAddPhoto.visibility = View.GONE
+        }
     }
     private fun showtoast(message: String) {
         Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
@@ -88,6 +132,73 @@ class RegisterActivity : AppCompatActivity() {
         } else {
             true
         }
+    }
+    private fun loading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.btnSignup.visibility = View.INVISIBLE
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.btnSignup.visibility = View.VISIBLE
+            binding.progressBar.visibility = View.INVISIBLE
+        }
+    }
+    private fun choixpromo() {
+        val checkedItem = intArrayOf(-1)
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setIcon(R.drawable.ic_launcher_background)
+        alertDialog.setTitle("Promotion")
+        val listItems = arrayOf(
+            "Preparatoire",
+            "L1",
+            "G2 ",
+            "GL",
+            "G2 MSI",
+            "G2 DSG",
+            "G2 AS",
+            "G2 TLC",
+            "G3 GL",
+            "G3 MSI",
+            "G3 DSG",
+            "G3 AS",
+            "G3 TLC",
+            " M1 AS-TLC",
+            "M1 DESIGN",
+            "M1 MIAGE"
+        )
+        alertDialog.setSingleChoiceItems(listItems, checkedItem[0]) { dialog, which ->
+            checkedItem[0] = which
+            val s = listItems[which]
+           binding.promotionChoice.text = s
+            dialog.dismiss()
+        }
+        alertDialog.setNegativeButton("Annuler") { dialog, which ->
+            dialog.dismiss()
+        }
+        val customAlertDialog = alertDialog.create()
+        customAlertDialog.show()
+    }
+    private fun choigenre(){
+
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Genre")
+        alertDialog.setPositiveButton("valide", null)
+        alertDialog.setNeutralButton("Annuler", null)
+        val items = arrayOf("Homme", "Femme")
+        val checkItem = 1
+        alertDialog.setSingleChoiceItems(
+            items, checkItem
+        ) { _: DialogInterface?, which: Int ->
+            when (which) {
+                0 -> {
+                    binding.genreChoice.text = "Homme"
+                }
+                1 -> {
+                    binding.genreChoice.text = "Femme"
+                }
+
+            }
+        }
+        alertDialog.show()
     }
 
 
