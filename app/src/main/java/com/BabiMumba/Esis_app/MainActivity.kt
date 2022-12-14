61f0e04791da8800b02ce9b23d733c1a9c3cb48f
Babi Mumba <babimumba243@gmail.com>
2022-12-14 23:19:08 +0200

    ade ne file

--- app/src/main/java/com/BabiMumba/Esis_app/MainActivity.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/MainActivity.kt
@@ -2,11 +2,22 @@ package com.BabiMumba.Esis_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val user_name = firebaseAuth.currentUser!!.email
+        name_maile.text = user_name.toString()
+    }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
     }
 }
