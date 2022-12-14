f83793f3b320501210140a5d9306b17b35a6b8c4
Babi Mumba <babimumba243@gmail.com>
2022-12-14 15:26:58 +0200

    add ew file

--- app/src/main/java/com/BabiMumba/Esis_app/Authentification/LoginActivity.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/Authentification/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.BabiMumba.Esis_app.Authentification
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.BabiMumba.Esis_app.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+    }
+    fun clicmethode(){
+        create_new_compte.setOnClickListener {
+            startActivity(Intent(this,RegisterActivity::class.java))
+        }
     }
 }
