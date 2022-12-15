82c095c75157f05dff7f65e336db043703206858
Babi Mumba <babimumba243@gmail.com>
2022-12-15 23:38:22 +0200

    add new fil e

--- app/src/main/java/com/BabiMumba/Esis_app/fragment/tab/L1Fragment.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/fragment/tab/L1Fragment.kt
@@ -1,21 +1,66 @@
 package com.BabiMumba.Esis_app.fragment.tab
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.BabiMumba.Esis_app.R
+import com.BabiMumba.Esis_app.adapters.syllabus_adapters
+import com.BabiMumba.Esis_app.home.PublicationSyllabus
+import com.BabiMumba.Esis_app.model.syllabus_model
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_l1.*
 
 class L1Fragment : Fragment() {
 
+
+    lateinit var myadaptes_syllabus: syllabus_adapters
+    lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_l1, container, false)
+        val v = inflater.inflate(R.layout.fragment_l1, container, false)
+        val recp = v.findViewById<RecyclerView>(R.id.recycler_l1)
+        v.findViewById<FloatingActionButton>(R.id.add_post).setOnClickListener {
+            startActivity(Intent(requireActivity(), PublicationSyllabus::class.java))
+        }
+
+        linearLayoutManager = LinearLayoutManager(activity)
+        linearLayoutManager.reverseLayout = true
+        linearLayoutManager.onSaveInstanceState()
+        linearLayoutManager.stackFromEnd = true
+
+        recp.layoutManager = linearLayoutManager
+        val options = FirebaseRecyclerOptions.Builder<syllabus_model>()
+            .setQuery(
+                FirebaseDatabase.getInstance().reference.child("syllabus").child("L1"),
+                syllabus_model::class.java
+            )
+            .build()
+        myadaptes_syllabus = syllabus_adapters(options)
+        recp.adapter = myadaptes_syllabus
+        myadaptes_syllabus.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+        myadaptes_syllabus.startListening()
+
+        return v
+    }
+    override fun onStart() {
+        recycler_l1.recycledViewPool.clear()
+        myadaptes_syllabus.notifyDataSetChanged()
+        super.onStart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        myadaptes_syllabus.stopListening()
     }
 
 }
