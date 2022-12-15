70efd1fe237c1884d130a0c49330eda2effa72b2
Babi Mumba <babimumba243@gmail.com>
2022-12-16 00:30:14 +0200

    create a new activity

---
+++ app/src/main/java/com/BabiMumba/Esis_app/users/ProfilUser.kt
@@ -0,0 +1,12 @@
+package com.BabiMumba.Esis_app.users
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.BabiMumba.Esis_app.R
+
+class ProfilUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_user)
+    }
+}
