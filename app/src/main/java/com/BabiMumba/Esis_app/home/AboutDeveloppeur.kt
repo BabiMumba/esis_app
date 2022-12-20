8f273745d2350e3a2d2af291f8667fe0c68a9d95
Babi Mumba <babimumba243@gmail.com>
2022-12-20 03:05:27 +0200

    new modification

--- app/src/main/java/com/BabiMumba/Esis_app/home/AboutDeveloppeur.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/home/AboutDeveloppeur.kt
@@ -1,10 +1,18 @@
 package com.BabiMumba.Esis_app.home
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.BabiMumba.Esis_app.R
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.android.synthetic.main.activity_about_developpeur.*
 
 class AboutDeveloppeur : AppCompatActivity() {
@@ -26,12 +34,7 @@ class AboutDeveloppeur : AppCompatActivity() {
             i.data = Uri.parse("https://api.whatsapp.com/send?phone=243975937553")
             startActivity(i)
         }
-        call_btn.setOnClickListener {
-            val nume = "tel:+243975937553"
-            val i = Intent(Intent.ACTION_CALL)
-            i.data=Uri.parse(nume)
-            startActivity(i)
-        }
+
         gith.setOnClickListener {
             val url = "https://github.com/BabiMumba"
             val i = Intent(Intent.ACTION_VIEW)
@@ -43,5 +46,35 @@ class AboutDeveloppeur : AppCompatActivity() {
             i.data = Uri.parse("mailto:babimumba243@gmail.com")
             startActivity(i)
         }
+        call_btn.setOnClickListener {
+            Dexter.withContext(
+                applicationContext
+            )
+                .withPermission(Manifest.permission.CALL_PHONE)
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(permissionGrantedResponse: PermissionGrantedResponse) {
+                        val nume = "tel:+243975937553"
+                        val i = Intent(Intent.ACTION_CALL)
+                        i.data=Uri.parse(nume)
+                        startActivity(i)
+                    }
+
+                    override fun onPermissionDenied(permissionDeniedResponse: PermissionDeniedResponse) {
+                        Toast.makeText(
+                            this@AboutDeveloppeur,
+                            "vous devez accepter pour continuer",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissionRequest: PermissionRequest,
+                        permissionToken: PermissionToken
+                    ) {
+                        permissionToken.continuePermissionRequest()
+                    }
+                }).check()
+        }
+
+        }
     }
-}
