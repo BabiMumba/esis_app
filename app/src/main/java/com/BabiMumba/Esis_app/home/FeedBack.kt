5619a9b30868dd12c7ddf52978d1be92e91fa8a1
Babi Mumba <babimumba243@gmail.com>
2022-12-20 23:59:23 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/home/FeedBack.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/home/FeedBack.kt
@@ -2,12 +2,45 @@ package com.BabiMumba.Esis_app.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.BabiMumba.Esis_app.R
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_feed_back.*
 
 class FeedBack : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed_back)
+
+    }
+    fun loading(isLoading: Boolean){
+        if (isLoading){
+            btn_send.visibility = View.GONE
+            progress_bar.visibility = View.VISIBLE
+        }else{
+            btn_send.visibility = View.VISIBLE
+            progress_bar.visibility = View.GONE
+        }
+    }
+    fun sen_message_feedback(){
+        loading(true)
+        val data:MutableMap<String,Any> = HashMap()
+        val nom = intent.getStringExtra("")
+        data["message"] = "$nom : ${txt_feedback.text}"
+        val db = FirebaseFirestore.getInstance()
+        // val message = db.collection("feedback").document(nom)
+        db.collection("feedback")
+            .add(data)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    loading(false)
+
+                }else{
+                    loading(false)
+                    Toast.makeText(this, "erreur", Toast.LENGTH_SHORT).show()
+                }
+            }
 
     }
 }
