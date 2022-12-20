dcffa3e8e247480058dfa4b444be47debd57bdf3
Babi Mumba <babimumba243@gmail.com>
2022-12-20 03:23:07 +0200

    add enw fil

---
+++ app/src/main/java/com/BabiMumba/Esis_app/home/FeedBack.kt
@@ -0,0 +1,13 @@
+package com.BabiMumba.Esis_app.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.BabiMumba.Esis_app.R
+
+class FeedBack : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed_back)
+
+    }
+}
