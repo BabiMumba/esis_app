9a2ccc0902f674229122d8d779f3ba05329b9aa2
Babi Mumba <babimumba243@gmail.com>
2022-12-21 07:19:02 +0200

    add enw file

--- app/src/main/java/com/BabiMumba/Esis_app/home/SplashScreen.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/home/SplashScreen.kt
@@ -1,14 +1,29 @@
 package com.BabiMumba.Esis_app.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.BabiMumba.Esis_app.Authentification.LoginActivity
 import com.BabiMumba.Esis_app.R
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        firebaseAuth = FirebaseAuth.getInstance()
         supportActionBar?.hide()
+        chek_users()
 
+    }
+    fun chek_users(){
+        if (firebaseAuth.currentUser != null){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
     }
 }
