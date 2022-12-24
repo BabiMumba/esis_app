07fc6a4b8ae3335dcfbbd606dadea20494f60b45
Babi Mumba <babimumba243@gmail.com>
2022-12-25 01:36:38 +0200

    add new fiel

---
+++ app/src/main/java/com/BabiMumba/Esis_app/admin/AllusersActivity.kt
@@ -0,0 +1,13 @@
+package com.BabiMumba.Esis_app.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.BabiMumba.Esis_app.R
+
+class AllusersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_allusers)
+
+    }
+}
