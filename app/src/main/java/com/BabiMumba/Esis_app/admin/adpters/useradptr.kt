3354ca4612f8ee8d1453ade3ab969dba7e00c5aa
Babi Mumba <babimumba243@gmail.com>
2022-12-25 01:47:24 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/admin/adpters/useradptr.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/admin/adpters/useradptr.kt
@@ -4,16 +4,27 @@ package com.BabiMumba.Esis_app.admin.adpters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.BabiMumba.Esis_app.R
 import com.BabiMumba.Esis_app.admin.model.modeluser
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import de.hdodenhof.circleimageview.CircleImageView
 
 class useradptr(options: FirebaseRecyclerOptions<modeluser>):FirebaseRecyclerAdapter<modeluser,useradptr.viewholder>(options) {
 
+
     inner  class  viewholder(item: View):RecyclerView.ViewHolder(item){
 
+        var image:CircleImageView
+        var name:TextView
+        var mail:TextView
+        init {
+            image = item.findViewById(R.id.image_profil_rod)
+            name = item.findViewById(R.id.name_user)
+            mail = item.findViewById(R.id.mail_user)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): useradptr.viewholder {
