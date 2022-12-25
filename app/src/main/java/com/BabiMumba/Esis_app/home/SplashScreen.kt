23cfec6780868645b3c1911fb69ceb6b268055bf
Babi Mumba <babimumba243@gmail.com>
2022-12-25 04:00:40 +0200

    add enw file

--- app/src/main/java/com/BabiMumba/Esis_app/home/SplashScreen.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/home/SplashScreen.kt
@@ -1,10 +1,12 @@
 package com.BabiMumba.Esis_app.home
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.widget.Toast
 import com.BabiMumba.Esis_app.Authentification.LoginActivity
 import com.BabiMumba.Esis_app.R
 import com.google.firebase.auth.FirebaseAuth
@@ -19,6 +21,15 @@ class SplashScreen : AppCompatActivity() {
         supportActionBar?.hide()
         Handler().postDelayed(
             {
+                val sharedPreferences = getSharedPreferences("info_users", Context.MODE_PRIVATE)
+                val name = sharedPreferences.getString("premium",null)
+
+                if (name=="oui"){
+                    Toast.makeText(this, "vous etes en mode payant", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this, "vous etes en mode grauit", Toast.LENGTH_SHORT).show()
+                }
+
                 slogan.visibility = View.VISIBLE
                 Handler().postDelayed({
                   chek_users()
