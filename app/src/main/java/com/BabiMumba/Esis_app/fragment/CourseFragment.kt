b2b40777b89297ddfb25163097b575e08153ffe4
Babi Mumba <babimumba243@gmail.com>
2023-01-24 21:05:03 +0200

    add enw file

--- app/src/main/java/com/BabiMumba/Esis_app/fragment/CourseFragment.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/fragment/CourseFragment.kt
@@ -1,21 +1,102 @@
 package com.BabiMumba.Esis_app.fragment
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.BabiMumba.Esis_app.R
+import com.BabiMumba.Esis_app.home.DocumentActivity2
+import com.BabiMumba.Esis_app.pdf.Pdf_listener_file
+import com.BabiMumba.Esis_app.pdf.pdfAdapter2
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import kotlinx.android.synthetic.main.activity_lecture_pdf.*
+import java.io.File
 
-class CourseFragment : Fragment() {
+class CourseFragment : Fragment(), Pdf_listener_file {
+
+    lateinit var pdfAdapter: pdfAdapter2
+    private lateinit var pdfList: MutableList<File>
+    private lateinit var recyclerView: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_course, container, false)
-
+        runtimePermissions(v)
         return v
+    }
+    private fun runtimePermissions(view: View) {
+        Dexter.withContext(requireActivity())
+            .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(permissionGrantedResponse: PermissionGrantedResponse) {
+                    displaypdf(view)
+                }
+                override fun onPermissionDenied(permissionDeniedResponse: PermissionDeniedResponse) {
+                    Toast.makeText(
+                        requireActivity(),
+                        "la permission est necessaire pour continuer",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissionRequest: PermissionRequest,
+                    permissionToken: PermissionToken
+                ) {
+                    permissionToken.continuePermissionRequest()
+                }
+            }).check()
+    }
+    fun findpdf(file: File): ArrayList<File> {
+        val arrayList = ArrayList<File>()
+        val files = file.listFiles()
+        for (singlefile in files) {
+            if (singlefile.isDirectory && !singlefile.isHidden) {
+                arrayList.addAll(findpdf(singlefile))
+            } else {
+                if (singlefile.name.endsWith(".pdf")) {
+                    arrayList.add(singlefile)
+                }
+            }
+        }
+        return arrayList
+    }
+    private fun displaypdf(view: View) {
+        recyclerView = view.findViewById(R.id.my_recyclerview)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity(),)
+        pdfList = ArrayList()
+        pdfList.addAll(
+            findpdf( File(
+                Environment.getExternalStorageDirectory()
+                    .toString() + "/" + Environment.DIRECTORY_DOWNLOADS + "/syllabus esis/"
+            ))
+
+        )
+        pdfAdapter = pdfAdapter2(requireActivity(), pdfList, this)
+        my_recyclerview.adapter = pdfAdapter
+    }
+    override fun onSelected(file: File?) {
+        if (file != null) {
+            startActivity(
+                Intent(requireActivity(), DocumentActivity2::class.java)
+                    .putExtra("path", file.absolutePath)
+            )
+        }
     }
 
 }
