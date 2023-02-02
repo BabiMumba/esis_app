3347408923845d4e563d0aafb109a84457ef697f
Babi Mumba <babimumba243@gmail.com>
2023-02-02 15:29:53 +0200

    add enw file

---
+++ app/src/main/java/com/BabiMumba/Esis_app/home/SyllabusPromo.kt
@@ -0,0 +1,11 @@
+package com.BabiMumba.Esis_app.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SyllabusPromo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_syllabus_promo)
+    }
+}
