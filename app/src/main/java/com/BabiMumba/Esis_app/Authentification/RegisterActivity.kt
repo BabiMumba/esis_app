dd5618110110abfcecb134ce74f9b5d390beb30f
Babi Mumba <babimumba243@gmail.com>
2023-02-05 00:35:37 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/Authentification/RegisterActivity.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/Authentification/RegisterActivity.kt
@@ -150,6 +150,11 @@ class RegisterActivity : AppCompatActivity() {
         infor_user["administrateur"] = "non"
         infor_user["adminP"] = "non"
         infor_user["ouverture_application"] = 1
+        infor_user["id_reserve1"] = ""
+        infor_user["id_reserve2"] = ""
+        infor_user["id_reserve3"] = ""
+        infor_user["id_reserve4"] = ""
+
         database.collection("Utilisateurs")
             .document(inputMail)
             .set(infor_user)
