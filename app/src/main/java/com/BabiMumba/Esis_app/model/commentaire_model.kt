1f6e2d9536ec7680b22ff615d69750a522fcf780
Babi Mumba <babimumba243@gmail.com>
2023-02-05 00:18:42 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/model/commentaire_model.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/model/commentaire_model.kt
@@ -16,12 +16,20 @@ class commentaire_model {
         nom: String,
         date: String,
         commentaire: String,
-        profil:String
+        profil:String,
+        id_reserve1:String,
+        id_reserve2:String,
+        id_reserve3:String,
+        id_reserve4:String,
     ) {
         this.nom = nom
         this.date = date
         this.commentaire = commentaire
         this.profil = profil
+        this.id_reserve1 = id_reserve1
+        this.id_reserve2 = id_reserve2
+        this.id_reserve3 = id_reserve3
+        this.id_reserve4 = id_reserve4
     }
 
 }
