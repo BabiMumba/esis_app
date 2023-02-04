b5db58da0f006254c14a410ccfc8ecd3b4e150bf
Babi Mumba <babimumba243@gmail.com>
2023-02-04 23:47:05 +0200

    add new ile

--- app/src/main/java/com/BabiMumba/Esis_app/home/PublicationSyllabus.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/home/PublicationSyllabus.kt
@@ -345,8 +345,7 @@ class PublicationSyllabus : AppCompatActivity() {
     fun save_syllabus_mprfl(lien:String, nom_livre:String, id_post:String){
         val firebaseUser = firebaseAuth.currentUser
         val mail = firebaseUser?.email.toString().replaceAfter("@"," ")
-
-        val data = save_profil_syllabus(lien,nom_livre,id_post)
+        val data = save_profil_syllabus(lien,nom_livre,id_post,"","","")
         databaseReference = FirebaseDatabase.getInstance().getReference("syllabus_poste_save")
         databaseReference.child(mail).child(id_post).setValue(data)
             .addOnCompleteListener {
--- app/src/main/java/com/BabiMumba/Esis_app/model/save_profil_syllabus.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/model/save_profil_syllabus.kt
@@ -5,16 +5,26 @@ class save_profil_syllabus {
     lateinit var nom_livre:String
     lateinit var lien_livre:String
     lateinit var id_poste:String
+    lateinit var id_res1:String
+    lateinit var id_res2:String
+    lateinit var id_res3:String
+
 
     constructor(){}
     constructor(
         lien_livre:String,
         nom_livre: String,
         id_poste: String,
+        id_res1: String,
+        id_res2: String,
+        id_res3: String,
     ) {
         this.lien_livre = lien_livre
         this.nom_livre = nom_livre
         this.id_poste = id_poste
+        this.id_res1 = id_res1
+        this.id_res2 = id_res2
+        this.id_res3 = id_res3
 
     }
 
