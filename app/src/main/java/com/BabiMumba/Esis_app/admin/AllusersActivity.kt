56444973e3fba3ef2c5088811e326695687f355c
Babi Mumba <babimumba243@gmail.com>
2023-02-05 23:51:50 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/admin/AllusersActivity.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/admin/AllusersActivity.kt
@@ -2,13 +2,17 @@ package com.BabiMumba.Esis_app.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.BabiMumba.Esis_app.R
 import com.BabiMumba.Esis_app.admin.adpters.useradptr
 import com.BabiMumba.Esis_app.admin.model.modeluser
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.android.gms.ads.*
 import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAd
+import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAdLoadCallback
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.Query
 import com.google.firebase.firestore.FirebaseFirestore
@@ -30,6 +34,14 @@ class AllusersActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_allusers)
 
+        MobileAds.initialize(this){
+            Log.d(TAG,"Oncreate:")
+        }
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder()
+                .setTestDeviceIds(listOf("e3ecfe91-a277-4650-92e0-4f0cf2ad9c13","1bda7af6-ef75-48d9-a0d7-2ea9121c42e6"))
+                .build()
+        )
         firebaseAuth = FirebaseAuth.getInstance()
        layoutManager = LinearLayoutManager(this@AllusersActivity)
         users_recyclerview.layoutManager = layoutManager
@@ -45,6 +57,82 @@ class AllusersActivity : AppCompatActivity() {
         adaps = useradptr(options)
         users_recyclerview.adapter = adaps
         }
+
+    private fun loadrewardedInters() {
+
+        RewardedInterstitialAd.load(
+            this,
+            resources.getString(com.google.android.gms.ads.R.string.rewarded_ad_reel),
+            //AdRequest adRequest = new AdRequest.Builder().addTestDevice("94DF0193F80DB5F14BFF0EA958D02BC9").build();
+            AdRequest.Builder().build(),
+
+            object : RewardedInterstitialAdLoadCallback(){
+                override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                    super.onAdFailedToLoad(loadAdError)
+                    Log.d(TAG,"chargement echouer:${loadAdError.message}")
+                    mRewardedInterstitialAd = null
+                }
+
+                override fun onAdLoaded(rewardedInterstitialAd: RewardedInterstitialAd) {
+                    super.onAdLoaded(rewardedInterstitialAd)
+                    Log.d(TAG,"OnLoaded")
+                    Toast.makeText(this@RewardiActivity, "video pret", Toast.LENGTH_SHORT).show()
+                    mRewardedInterstitialAd = rewardedInterstitialAd
+                }
+            }
+
+        )
+    }
+    private fun showRewardedInters(){
+
+        if (mRewardedInterstitialAd != null){
+            Log.d(TAG,"showRewarded")
+            mRewardedInterstitialAd!!.fullScreenContentCallback =
+                object: FullScreenContentCallback(){
+                    override fun onAdClicked() {
+                        super.onAdClicked()
+
+                        Log.d(TAG,"onAdclicked")
+                        Toast.makeText(this@AllusersActivity, "vous avez cliquez sur la pub", Toast.LENGTH_SHORT).show()
+
+                    }
+                    override fun onAdDismissedFullScreenContent() {
+                        super.onAdDismissedFullScreenContent()
+                        Toast.makeText(this@AllusersActivity, "Vous avez fermer ", Toast.LENGTH_SHORT).show()
+                        Log.d(TAG,"onAddismissFullScreen: ")
+
+                        mRewardedInterstitialAd = null
+                        loadrewardedInters()
+
+                    }
+
+                    override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                        super.onAdFailedToShowFullScreenContent(adError)
+                        Log.d(TAG,"onAdfailedtoShowFullscreen: ${adError.message} ")
+                        mRewardedInterstitialAd = null
+                    }
+                    override fun onAdImpression() {
+                        super.onAdImpression()
+                        Toast.makeText(this@AllusersActivity, "pub valider", Toast.LENGTH_SHORT).show()
+                        Log.d(TAG,"onAdImpression: ")
+                    }
+                    override fun onAdShowedFullScreenContent() {
+                        super.onAdShowedFullScreenContent()
+                        Log.d(TAG,"onAdShowFullScreencontente: ")
+                    }
+
+                }
+            mRewardedInterstitialAd!!.show(this){
+              //  ajouter()
+                Toast.makeText(this, "recompence accorder", Toast.LENGTH_SHORT).show()
+                Log.d(TAG,"onUserEarnedrewarded: ")
+            }
+        }
+        else{
+            Log.d(TAG,"chargement echouer")
+            Toast.makeText(this, "chargement echouer", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onStart() {
         adaps.startListening()
