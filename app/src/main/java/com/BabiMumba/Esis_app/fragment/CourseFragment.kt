a3f7b87dfc46b30a6ae97b588d38b7c83cdba2b4
Babi Mumba <babimumba243@gmail.com>
2023-02-06 10:37:46 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/fragment/CourseFragment.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/fragment/CourseFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,8 +14,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.BabiMumba.Esis_app.R
 import com.BabiMumba.Esis_app.home.DocumentActivity2
+import com.BabiMumba.Esis_app.home.LectureActivity_Pdf
 import com.BabiMumba.Esis_app.pdf.Pdf_listener_file
 import com.BabiMumba.Esis_app.pdf.pdfAdapter2
+import com.google.android.gms.ads.*
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -30,12 +33,62 @@ class CourseFragment : Fragment(), Pdf_listener_file {
     private lateinit var pdfList: MutableList<File>
     private lateinit var recyclerView: RecyclerView
     lateinit var linearLayoutManager: LinearLayoutManager
+    private companion object{
+        private const val TAG = "BANNER_AD_TAG"
+    }
+    private var adview: AdView? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_course, container, false)
+
+        MobileAds.initialize(requireActivity()){
+            Log.d(TAG,"inias complet")
+        }
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder()
+                .setTestDeviceIds(listOf("f01a4b37-2568-4128-9894-6d6453fd67bb"))
+                .build()
+        )
+        adview = v.findViewById(R.id.bannerAd)
+        val adRequest = AdRequest.Builder().build()
+        adview?.loadAd(adRequest)
+        adview?.adListener = object : AdListener() {
+            override fun onAdClicked() {
+                super.onAdClicked()
+                Log.d(TAG, "onAdClicked: ")
+            }
+
+            override fun onAdClosed() {
+                super.onAdClosed()
+                Log.d(TAG, "onAdClosed: ")
+            }
+
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                super.onAdFailedToLoad(p0)
+                Log.d(TAG, "onAdFailedToLoad: $p0")
+            }
+
+            override fun onAdImpression() {
+                super.onAdImpression()
+                Log.d(TAG, "onAdImpression: ")
+            }
+
+            override fun onAdLoaded() {
+                super.onAdLoaded()
+                Log.d(TAG, "onAdLoaded: ")
+            }
+
+            override fun onAdOpened() {
+                super.onAdOpened()
+                Log.d(TAG, "onAdOpened: ")
+            }
+        }
+
+
         try {
             runtimePermissions(v)
         }catch (e:Exception){
