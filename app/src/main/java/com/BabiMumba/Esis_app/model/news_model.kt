5ec02151a0cdce4ad0db23b42f4548558e5a873c
Babi Mumba <babimumba243@gmail.com>
2023-02-12 22:29:31 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/model/news_model.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/model/news_model.kt
@@ -5,17 +5,23 @@ class news_model {
     lateinit var message:String
     lateinit var date:String
     lateinit var image:String
+    lateinit var id_doc:String
+    lateinit var autor:String
     constructor(){}
     constructor(
         titre: String,
         message: String,
         date: String,
         image:String,
+        id_doc:String,
+        autor:String,
     ) {
         this.titre = titre
         this.message = message
         this.date = date
         this.image = image
+        this.id_doc = id_doc
+        this.autor = autor
 
     }
 }
