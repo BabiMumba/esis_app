d6c456a5982564a310edeb76dd4c64720e230906
Babi Mumba <babimumba243@gmail.com>
2023-02-12 18:33:28 +0200

    add enw file

---
+++ app/src/main/java/com/BabiMumba/Esis_app/home/DetailleNews.kt
@@ -0,0 +1,11 @@
+package com.BabiMumba.Esis_app.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailleNews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detaille_news)
+    }
+}
