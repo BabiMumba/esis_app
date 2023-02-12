df0644b6264b68c403954130774262d03c577dd8
Babi Mumba <babimumba243@gmail.com>
2023-02-12 19:04:48 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/home/DetailleNews.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/home/DetailleNews.kt
@@ -2,7 +2,12 @@ package com.BabiMumba.Esis_app.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.BabiMumba.Esis_app.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import kotlinx.android.synthetic.main.activity_detaille_news.*
+import kotlinx.android.synthetic.main.content_news.*
 
 class DetailleNews : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +17,24 @@ class DetailleNews : AppCompatActivity() {
         val message = intent.getStringExtra("message")
         val date = intent.getStringExtra("date")
         val image = intent.getStringExtra("image")
+
+
+        promot_news.text = "toute les promotions"
+        title_news.text = titre
+        descri.text = message
+        date_news.text = date
+        val circularProgressDrawable = CircularProgressDrawable(this)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.start()
+        Glide
+            .with(this)
+            .load(image)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            //.apply(RequestOptions.overrideOf(300,600))
+            .centerCrop()
+            .placeholder(circularProgressDrawable)
+            .into(news_image_id)
 
 
     }
