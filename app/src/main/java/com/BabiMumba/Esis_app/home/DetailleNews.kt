ca7ea6454f78fc4d88104274a19b3f417e84c1cf
Babi Mumba <babimumba243@gmail.com>
2023-02-12 18:37:35 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/home/DetailleNews.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/home/DetailleNews.kt
@@ -2,10 +2,12 @@ package com.BabiMumba.Esis_app.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.BabiMumba.Esis_app.R
 
 class DetailleNews : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detaille_news)
+
     }
 }
