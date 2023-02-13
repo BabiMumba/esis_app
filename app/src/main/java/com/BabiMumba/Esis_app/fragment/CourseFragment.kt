9298cbd5622ae0a15b4c9d12356baaaaaf6ed147
Babi Mumba <babimumba243@gmail.com>
2023-02-13 16:25:57 +0200

    ad new file

--- app/src/main/java/com/BabiMumba/Esis_app/fragment/CourseFragment.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/fragment/CourseFragment.kt
@@ -141,13 +141,14 @@ class CourseFragment : Fragment(), Pdf_listener_file {
         linearLayoutManager.stackFromEnd = true
         recyclerView.layoutManager = linearLayoutManager
         pdfList = ArrayList()
-        pdfList.addAll(
-            findpdf( File(
+        pdfList.addAll( findpdf( File(Environment.getExternalStorageDirectory().Environment.DIRECTORY_DOWNLOADS+"/")));
+        /*
+        findpdf( File(
                 Environment.getExternalStorageDirectory()
                     .toString() + "/" + Environment.DIRECTORY_DOWNLOADS + "/syllabus esis/"
             ))
 
-        )
+         */
         pdfAdapter = pdfAdapter2(requireActivity(), pdfList, this)
         recyclerView.adapter = pdfAdapter
     }
