16e408834c35c05afcaedb86c4a6be9cd76b3d59
Babi Mumba <babimumba243@gmail.com>
2023-02-14 21:51:28 +0200

    add enw file

--- app/src/main/java/com/BabiMumba/Esis_app/home/LectureActivity_Pdf.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/home/LectureActivity_Pdf.kt
@@ -101,19 +101,6 @@ class LectureActivity_Pdf : AppCompatActivity(), Pdf_listener_file {
 
 
     }
-
-
-    override fun onPause() {
-        adview?.pause()
-        super.onPause()
-        Log.d(TAG, "onPause: ")
-
-    }
-    override fun onResume() {
-        adview?.resume()
-        super.onResume()
-        Log.d(TAG, "onResume: ")
-    }
     private fun runtimePermissions() {
         Dexter.withContext(this@LectureActivity_Pdf)
             .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
--- app/src/main/java/com/BabiMumba/Esis_app/home/SyllabusPromo.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/home/SyllabusPromo.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
@@ -15,6 +16,7 @@ import com.BabiMumba.Esis_app.R
 import com.BabiMumba.Esis_app.adapters.syllabus_adapters
 import com.BabiMumba.Esis_app.model.syllabus_model
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.android.gms.ads.*
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_actualite.*
 import kotlinx.android.synthetic.main.activity_syllabus_promo.*
@@ -26,9 +28,59 @@ class SyllabusPromo : AppCompatActivity() {
     lateinit var myadaptes_syllabus: syllabus_adapters
     lateinit var linearLayoutManager: LinearLayoutManager
 
+    private companion object{
+        private const val TAG = "BANNER_AD_TAG"
+    }
+    private var adview: AdView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_syllabus_promo)
+
+        MobileAds.initialize(this){
+            Log.d(TAG,"inias complet")
+        }
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder()
+                .setTestDeviceIds(listOf("f01a4b37-2568-4128-9894-6d6453fd67bb"))
+                .build()
+        )
+        adview = findViewById(R.id.bannerAd)
+        val adRequest = AdRequest.Builder().build()
+        adview?.loadAd(adRequest)
+        adview?.adListener = object : AdListener() {
+            override fun onAdClicked() {
+                super.onAdClicked()
+                Log.d(TAG, "onAdClicked: ")
+            }
+
+            override fun onAdClosed() {
+                super.onAdClosed()
+                Log.d(TAG, "onAdClosed: ")
+            }
+
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                super.onAdFailedToLoad(p0)
+                Log.d(TAG, "onAdFailedToLoad: $p0")
+            }
+
+            override fun onAdImpression() {
+                super.onAdImpression()
+                Log.d(TAG, "onAdImpression: ")
+                Toast.makeText(this@SyllabusPromo, "impression effectuer", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAdLoaded() {
+                super.onAdLoaded()
+                Log.d(TAG, "onAdLoaded: ")
+            }
+
+            override fun onAdOpened() {
+                super.onAdOpened()
+                Log.d(TAG, "onAdOpened: ")
+            }
+        }
+
 
         if (isConnectedNetwork(this)){
             //connecter
