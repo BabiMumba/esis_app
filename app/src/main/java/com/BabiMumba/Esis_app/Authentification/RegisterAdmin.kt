dcf6cd90f7480f5df81212bcc327a177869e26d8
Babi Mumba <babimumba243@gmail.com>
2023-02-15 08:49:40 +0200

    add new activity

---
+++ app/src/main/java/com/BabiMumba/Esis_app/Authentification/RegisterAdmin.kt
@@ -0,0 +1,13 @@
+package com.BabiMumba.Esis_app.Authentification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.BabiMumba.Esis_app.R
+
+class RegisterAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_admin)
+
+    }
+}
