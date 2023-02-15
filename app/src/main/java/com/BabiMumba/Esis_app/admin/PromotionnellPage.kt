d86a46bb02a15aa1f17993faf207ea7e1f277908
Babi Mumba <babimumba243@gmail.com>
2023-02-15 22:31:07 +0200

    add enw file

---
+++ app/src/main/java/com/BabiMumba/Esis_app/admin/PromotionnellPage.kt
@@ -0,0 +1,12 @@
+package com.BabiMumba.Esis_app.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.BabiMumba.Esis_app.R
+
+class PromotionnellPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_promotionnell_page)
+    }
+}
