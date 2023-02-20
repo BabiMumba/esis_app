9705f99e22ae23017cbad932c7ee40dbccc6f7d4
Babi Mumba <babimumba243@gmail.com>
2023-02-20 21:59:29 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/admin/AdministateurPage.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/admin/AdministateurPage.kt
@@ -2,12 +2,48 @@ package com.BabiMumba.Esis_app.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.BabiMumba.Esis_app.R
+import com.BabiMumba.Esis_app.admin.adpters.useradptr
+import com.BabiMumba.Esis_app.admin.model.modeluser
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_adminpage.*
 
 class AdministateurPage : AppCompatActivity() {
+    private var layoutManager: LinearLayoutManager? = null
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var adaps: useradptr
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_administateur_page)
+        firebaseAuth = FirebaseAuth.getInstance()
+        layoutManager = LinearLayoutManager(this@AdministateurPage)
+        admin_recyclerview.layoutManager = layoutManager
+        //Rreference des utilisateur
+        val ref = FirebaseFirestore.getInstance().collection("Utilisateurs")
+        val options = FirestoreRecyclerOptions.Builder<modeluser>()
+            .setQuery(
+                ref,
+                modeluser::class.java
+            )
+            .build()
+        adaps = useradptr(options)
+        admin_recyclerview.adapter = adaps
 
+    }
+
+    override fun onStart() {
+        adaps.startListening()
+        admin_recyclerview.recycledViewPool.clear()
+        adaps.notifyDataSetChanged()
+        super.onStart()
+    }
+
+    override fun onStop() {
+        adaps.stopListening()
+        super.onStop()
     }
 }
