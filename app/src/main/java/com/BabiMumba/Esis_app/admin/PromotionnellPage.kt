33d6a12fc6e03faa64ed774aece1134f68fca996
Babi Mumba <babimumba243@gmail.com>
2023-02-20 21:44:33 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/admin/PromotionnellPage.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/admin/PromotionnellPage.kt
@@ -2,11 +2,21 @@ package com.BabiMumba.Esis_app.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.BabiMumba.Esis_app.R
+import com.BabiMumba.Esis_app.admin.adpters.useradptr
+import com.google.firebase.auth.FirebaseAuth
 
 class PromotionnellPage : AppCompatActivity() {
+
+    private var layoutManager: LinearLayoutManager?= null
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var adaps: useradptr
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_promotionnell_page)
+
     }
 }
