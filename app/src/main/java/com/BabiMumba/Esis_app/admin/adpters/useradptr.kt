973b06764ff425711cc6dcf1529cf42cf3c1459f
Babi Mumba <babimumba243@gmail.com>
2023-02-26 23:10:17 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/admin/adpters/useradptr.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/admin/adpters/useradptr.kt
@@ -107,6 +107,8 @@ class useradptr (options: FirestoreRecyclerOptions<modeluser>):
                         alertDialog.setTitle("Administrateur assistant")
                         alertDialog.setMessage("""
                             en ajoutant $nom en tant qu'administrateur assistant il aura la possibilite de \n
+                            a)publier et supprimer des syllabus
+                            b)publier un communique
                             
                         """.trimIndent())
                         alertDialog.setButton(AlertDialog.BUTTON_POSITIVE,"ajouter"){
