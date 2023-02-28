671a991d050b4597b4801585317e115ea3d51cf5
Babi Mumba <babimumba243@gmail.com>
2023-02-28 21:34:40 +0200

    add new file

--- app/src/main/java/com/BabiMumba/Esis_app/chat/ChatActivity.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/chat/ChatActivity.kt
@@ -3,11 +3,13 @@ package com.BabiMumba.Esis_app.chat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.BabiMumba.Esis_app.R
+import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
         val nom = intent.getStringExtra("nom").toString()
+        name_User.text = nom
     }
 }
