377896c5baf3c27f5e9240be91c1fb2a0fcf2dd7
Babi Mumba <babimumba243@gmail.com>
2023-02-28 09:49:50 +0200

    create a new class

---
+++ app/src/main/java/com/BabiMumba/Esis_app/users/Profil_DetailUser.kt
@@ -0,0 +1,12 @@
+package com.BabiMumba.Esis_app.users
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.BabiMumba.Esis_app.R
+
+class Profil_DetailUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_detail_user)
+    }
+}
