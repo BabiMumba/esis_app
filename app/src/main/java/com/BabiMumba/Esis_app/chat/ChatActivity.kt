3e2021ed35bbb28305f3c024a0966fabd96030ff
Babi Mumba <babimumba243@gmail.com>
2023-03-01 14:47:07 +0200

    add enw file

--- app/src/main/java/com/BabiMumba/Esis_app/chat/ChatActivity.kt
+++ app/src/main/java/com/BabiMumba/Esis_app/chat/ChatActivity.kt
@@ -61,8 +61,8 @@ class ChatActivity : AppCompatActivity() {
         InputComment.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                send_comment.isVisible = item_input.text.toString().trim().isNotEmpty()
-                send_comment.setOnClickListener {
+                item_send.isVisible = item_input.text.toString().trim().isNotEmpty()
+                item_send.setOnClickListener {
 
                 }
             }
